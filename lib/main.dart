f9197a59383e39998c71fb7d9a5ccd7621f58b6f
IT21196560 <it21196560@my.sliit.lk>
2024-06-18 16:52:59 +0530

    added tones buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main(){
   
@@ -16,11 +17,104 @@ class XylophoneApp extends StatefulWidget {
 }
 
 class _XylophoneAppState extends State<XylophoneApp> {
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Text(adjectives.first)),
+        body: SafeArea(
+          child: Column(
+
+            children: [
+              TextButton(
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    await player.play(AssetSource("note1.wav"));
+                  },
+                child: Text(""),
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.red),
+                  fixedSize: WidgetStateProperty.all<Size>(Size.fromHeight(75.0)),
+
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource("note2.wav"));
+                },
+                child: Text(""),
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.red),
+                  fixedSize: WidgetStateProperty.all<Size>(Size.fromHeight(75.0)),
+
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource("note3.wav"));
+                },
+                child: Text(""),
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.red),
+                  fixedSize: WidgetStateProperty.all<Size>(Size.fromHeight(75.0)),
+
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource("note4.wav"));
+                },
+                child: Text(""),
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.red),
+                  fixedSize: WidgetStateProperty.all<Size>(Size.fromHeight(75.0)),
+
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource("note5.wav"));
+                },
+                child: Text(""),
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.red),
+                  fixedSize: WidgetStateProperty.all<Size>(Size.fromHeight(75.0)),
+
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource("note6.wav"));
+                },
+                child: Text(""),
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.red),
+                  fixedSize: WidgetStateProperty.all<Size>(Size.fromHeight(75.0)),
+
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource("note7.wav"));
+                },
+                child: Text(""),
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.red),
+                  fixedSize: WidgetStateProperty.all<Size>(Size.fromHeight(75.0)),
+
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
